f55d53d16053623712598468eb45227daa7c5c82
Zlagoviah <141197573+Zlagoviah@users.noreply.github.com>
2025-10-03 20:25:22 -0600

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -7,4 +7,4 @@ gc = gspread.service_account_from_dict(json.loads(creds_json))  # dict-based aut
 ws = gc.open("Concentrador de ventas").worksheet("Cotizaciones")  # share with client_email [web:95]
 df = pd.DataFrame(ws.get_all_records())  # rows to DataFrame [web:95]
 
-df.head()
+df.to_csv('df_desc.csv', index=False)
