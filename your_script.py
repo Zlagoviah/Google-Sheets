571ab38d79b246accf0fd6a195fd7683934f62c3
Zlagoviah <141197573+Zlagoviah@users.noreply.github.com>
2025-10-08 15:39:06 -0600

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -6,7 +6,7 @@ import pandas as pd
 import re
 from datetime import datetime
 
-# 1) auth & open sheet
+  # 1) auth & open sheet
 creds_json = os.environ["GOOGLE_CREDS_JSON"]  # secret from Actions [web:12]
 gc = gspread.service_account_from_dict(json.loads(creds_json))  # dict-based auth [web:95][web:101]
 ws = gc.open("Concentrador de ventas").worksheet("Cotizaciones")  # share with client_email [web:95]
